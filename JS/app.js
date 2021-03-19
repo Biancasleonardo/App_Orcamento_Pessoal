7c1615fd302b81c7a53c27d16b2404b5e34c9d52
biancasleonardo <biancasleonardo@gmail.com>
2021-03-19 15:34:21 -0300

    criacao da classe despesa

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,40 @@
-alert('ok')
+class Despesa {
+    constructor(ano, mes, dia, tipo, descricao, valor){
+        this.ano = ano
+        this.mes = mes
+        this.dia = dia
+        this.tipo = tipo
+        this.descricao = descricao
+        this.valor = valor
+    }
+}
+
+function cadastrarDespesa() {
+    
+    // recebe as variáveis 
+    let ano = document.getElementById('ano')
+    let mes = document.getElementById('mes')
+    let dia = document.getElementById('dia')
+    let tipo = document.getElementById('tipo')
+    let descricao = document.getElementById('descricao')
+    let valor = document.getElementById('valor')
+
+
+    // cria o objeto e recupera o valor das variáveis
+    let despesa = new Despesa(
+        ano.value, 
+        mes.value, 
+        dia.value, 
+        tipo.value, 
+        descricao.value, 
+        valor.value
+    )
+    
+    // chama a função gravar passando como parâmetro os valores de despesa
+    gravar(despesa)    
+}
+
+function gravar(d) {
+
+    localStorage.setItem('despesa', JSON.stringify(d))
+}
