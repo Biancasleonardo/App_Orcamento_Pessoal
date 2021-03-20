9e9814124fa3685cde39ea9610275561f4181cec
biancasleonardo <biancasleonardo@gmail.com>
2021-03-20 09:33:39 -0300

    inclusão de id e validação de dados

--- JS/app.js
+++ JS/app.js
@@ -7,7 +7,45 @@ class Despesa {
         this.descricao = descricao
         this.valor = valor
     }
+
+    // Validação de dados
+    validarDados(){
+        for (let i in this){
+            if (this[i] == undefined || this[i] == '' || this[i] == null) {
+                return false
+            }
+        }
+        return true
+    }
 }
+
+class Bd{
+    // cria o id quando não existe uma informação de ID
+    constructor(){
+        let id = localStorage.getItem('id')
+
+        if (id === null) {
+            localStorage.setItem('id', 0)
+        }
+    }
+    // verifica o Id
+    getProximoId(){
+        let proximoId = localStorage.getItem('id')
+        return parseInt(proximoId) + 1
+    }
+    // grava os dados em Json no banco de dados
+    gravar(d) {
+        // atribui o valor de getProximoId a id
+        let id = this.getProximoId()
+
+        localStorage.setItem(id, JSON.stringify(d))
+
+        // seta o valor contido em id no banco de dados
+        localStorage.setItem('id', id)
+    }
+}
+
+let bd = new Bd()
 
 function cadastrarDespesa() {
     
@@ -30,11 +68,14 @@ function cadastrarDespesa() {
         valor.value
     )
     
-    // chama a função gravar passando como parâmetro os valores de despesa
-    gravar(despesa)    
+    if(despesa.validarDados()){
+        // chama a função gravar passando como parâmetro os valores de despesa
+        bd.gravar(despesa)  
+        alert('Despesa Cadastrada com sucesso')
+    } else {
+        // dialog de erro
+        alert('Dados inválidos')
+    }
+      
 }
 
-function gravar(d) {
-
-    localStorage.setItem('despesa', JSON.stringify(d))
-}
