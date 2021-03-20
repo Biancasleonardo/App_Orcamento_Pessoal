cb82b731ee45b00f8a9bc068da951e562eae023a
biancasleonardo <biancasleonardo@gmail.com>
2021-03-20 17:24:26 -0300

    inclusão de consulta

--- JS/app.js
+++ JS/app.js
@@ -43,6 +43,32 @@ class Bd{
         // seta o valor contido em id no banco de dados
         localStorage.setItem('id', id)
     }
+
+    recuperarTodosRegistros(){
+        // array das despesas
+        let despesas = Array()
+
+        // recupera o id atual
+        let id = localStorage.getItem('id')
+
+        // recupera todas as despesas cadastradas em localStorage
+        for (let i = 1; i <= id; i++) {
+            
+            // recupera a despesa
+            let despesa = JSON.parse(localStorage.getItem(i))
+
+            // verifica se existe indices null ou apagados
+            if (despesa === null) {
+                continue
+            }
+
+            // inclui a despesa no array de despesas
+            despesas.push(despesa)
+            
+        }
+
+        return despesas
+    }
 }
 
 let bd = new Bd()
@@ -71,11 +97,77 @@ function cadastrarDespesa() {
     if(despesa.validarDados()){
         // chama a função gravar passando como parâmetro os valores de despesa
         bd.gravar(despesa)  
-        alert('Despesa Cadastrada com sucesso')
+
+        
+        document.getElementById('modal_titulo').innerHTML = 'Registro inserido com sucesso!'
+        document.getElementById('modal_titulo_div').className = 'modal-header text-success'
+
+        document.getElementById('modal_conteudo').innerHTML = 'Despesa foi cadastrada com sucesso!'
+
+        document.getElementById('modal_botao').innerHTML = 'Voltar'
+        document.getElementById('modal_botao').className = 'btn btn-success'
+
+        $('#modalRegistraDespesa').modal('show')
+
+        // Limpa os campos após ser gravado no bd
+        dia.value = ''
+        mes.value = ''
+        ano.value = ''
+        tipo.value = ''
+        descricao.value = ''
+        valor.value = ''
+
     } else {
+
+        
+        document.getElementById('modal_titulo').innerHTML = 'Despesa não registrada!'
+        document.getElementById('modal_titulo_div').className = 'modal-header text-danger'
+
+        document.getElementById('modal_conteudo').innerHTML = 'Erro na gravação, verifique se todos as informações foram preenchidas!'
+
+        document.getElementById('modal_botao').innerHTML = 'Voltar e corrigir'
+        document.getElementById('modal_botao').className = 'btn btn-danger'
+        
+
         // dialog de erro
-        alert('Dados inválidos')
+        $('#modalRegistraDespesa').modal('show')
     }
       
+}
+
+function carregaListaDespesas() {
+    let despesas = Array()
+
+    despesas = bd.recuperarTodosRegistros()
+
+    // passa as informações de desepesa para a tela
+    var listaDespesas = document.getElementById('listaDespesas')
+
+    // percorrer o array despesas, listando cada despesa de forma dinâmica
+    despesas.forEach(function(d){
+
+        // criando a linha (tr)
+        let linha = listaDespesas.insertRow()
+
+        // criar as colunas
+        linha.insertCell(0).innerHTML = `${d.dia}/${d.mes}/${d.ano}`
+        
+        // ajutar o tipo
+        switch (d.tipo) {
+            case '1': d.tipo = 'Alimentação'
+                break;
+            case '2': d.tipo = 'Educação'
+                break;
+            case '3': d.tipo = 'Lazer'
+                break;
+            case '4': d.tipo = 'Saúde'
+                break;
+            case '5': d.tipo = 'Transporte'
+                break;
+        }
+        linha.insertCell(1).innerHTML = d.tipo
+        linha.insertCell(2).innerHTML = d.descricao
+        linha.insertCell(3).innerHTML = d.valor
+    })
 }
 
